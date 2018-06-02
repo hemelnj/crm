759461f8f52ce4562074ccd96d2ecf89376d00e1
hemel <you@example.com>
2018-06-03 00:21:47 +0600

    Unit Test Added

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hemel
+ */
+
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class) 
+public class HelloWorldTest {
+    @Test
+    void firstTest() {
+        System.out.println("First test");
+    }
+    
+    @Test
+    void secondTest() {
+        System.out.println("second test");
+    }
+    
+    @Test
+    void thirdTest() {
+        System.out.println("third test");
+    }
+    @Test
+    void fourthTest() {
+        System.out.println("fourth Test");
+    }
+    @Test
+    void fifthTest() {
+        System.out.println("fifth Test");
+    }
+    
+    @Test
+    void sixthTest() {
+        System.out.println("fifth Test");
+    }
+    
+    @Test
+    void sevenTest() {
+        System.out.println("fifth Test");
+    }
+}
